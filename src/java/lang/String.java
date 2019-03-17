ddd6f7e2044ba0fbd0db2f89cd202ed41ac3e8c3
jxhmask <jxhmask@sina.com>
2019-03-17 11:15:26 +0800

    delete String

--- src/java/lang/String.java
+++
@@ -1,9 +0,0 @@
-package java.lang;
-
-public class String {
-	public static void main(String[] args) {
-		String str = new String();
-		str = "asdf";
-		System.out.println(str.toUpperCase());
-	}
-}
