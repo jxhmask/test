ddd7d403749133f968cce61e49faeffc2ec03860
jxhmask <jxhmask@sina.com>
2019-03-17 11:26:51 +0800

    commit

--- src/com/test/observer/AObserver.java
+++ src/com/test/observer/AObserver.java
@@ -17,7 +17,7 @@ public class AObserver implements Observer {
 	@Override
 	public void update(Observable o, Object arg) {
 		System.out.println("AObserver:" + ((ServerManager)o).getDate() + "; " + arg);
-		
+		//hello 
 	}
 
 }
